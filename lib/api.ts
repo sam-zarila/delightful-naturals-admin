dabf59738e8ecc6e7c201b64f128bfdc4f4f3a89
Omikhe <msakamike84@gmail.com>
2025-10-11 19:50:42 +0200

    fix: correct API_BASE URL to use HTTPS

--- lib/api.ts
+++ lib/api.ts
@@ -1,5 +1,5 @@
 // export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://localhost/delightful" ;
-export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "http://api.delightfulnaturals.co.za" ;
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE || "https://api.delightfulnaturals.co.za" ;
 
 
 async function request(path: string, opts: RequestInit = {}) {
